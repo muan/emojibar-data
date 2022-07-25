82b162101e594d6a8bd37b845e72fc5eec7ff1cf
Mu-An Chiou <me@muanchiou.com>
2022-07-25 18:28:13 +0800

    Enforce type

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const keywordSet = require('emojilib')
 const data = []
 
 for (const key in metadata) {
-  const emoji = metadata[key]
+  const emoji = {}
+
+  emoji['name'] = metadata[key]['name']
+  emoji['slug'] = metadata[key]['slug']
+  emoji['group'] = metadata[key]['group']
+  emoji['emoji_version'] = Number(metadata[key]['emoji_version'])
+  emoji['unicode_version'] = Number(metadata[key]['unicode_version'])
+  emoji['skin_tone_support'] = metadata[key]['skin_tone_support']
   emoji['value'] = key
   emoji['keywords'] = keywordSet[key]
   data.push(emoji)
